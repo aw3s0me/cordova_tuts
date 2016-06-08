90bf88447e521c5742c2b43e40b4ccb25e61c0f4
Aleksander Korovin <akorovin00@gmail.com>
2016-06-08 18:04:03 +0200

    Added HTML Views, dummy pages (Search, Post) + Controller

---
+++ www/js/controller.js
@@ -0,0 +1,62 @@
+/**
+ * Controller to switch between apps. Because we use SPA.
+ */
+var Controller = function() {
+    var controller = {
+        self: null,
+        initialize: function() {
+            self = this;
+            self.bindEvents();
+            self.renderSearchView();
+        },
+        bindEvents: function() {
+            $('.tab-button').on('click', this.onTabClick);
+        },
+        onTabClick: function(event) {
+            event.preventDefault();
+            if ($(this).hasClass('active')) {
+                return;
+            }
+
+            var tab = $(this).data('tab');
+            if (tab === '#add-tab') {
+                self.renderPostView();
+            } else {
+                self.renderSearchView();
+            }
+        },
+        /**
+         * Rendering Post View
+         * @return {[type]} [description]
+         */
+        renderPostView: function() {
+            $('.tab-button').removeClass('active');
+            $('#post-tab-button').addClass('active');
+
+            var $tab = $('#tab-content');
+            $tab.empty();
+            $("#tab-content").load("./views/post-project-view.html", function(data) {
+                $('#tab-content').find('#post-project-form').on('submit', self.postProject);
+            });
+        },
+        /**
+         * Rendering Search View
+         * @return {[type]} [description]
+         */
+        renderSearchView: function() {
+            $('.tab-button').removeClass('active');
+            $('#search-tab-button').addClass('active');
+
+            var $tab = $('#tab-content');
+            $tab.empty();
+
+            var $projectTemplate = null;
+            $("#tab-content").load("./views/search-project-view.html", function(data) {
+                $projectTemplate = $('.project').remove();
+                // Load projects here
+            });
+        }
+    }
+    controller.initialize();
+    return controller;
+}
--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var controller = null;
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,6 +35,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        controller = new Controller();
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
